01e54a0ed5505644d2bbc7b1fbb6c4285fb5fc97
yanxs <541139655@qq.com>
2018-03-06 09:41:02 +0800

    初始化

---
+++ application/extra/addons.php
@@ -0,0 +1,19 @@
+<?php
+
+return array (
+  'autoload' => false,
+  'hooks' => 
+  array (
+    'enable' => 
+    array (
+      0 => 'wechat',
+    ),
+    'disable' => 
+    array (
+      0 => 'wechat',
+    ),
+  ),
+  'route' => 
+  array (
+  ),
+);
---
+++ runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
@@ -0,0 +1,4 @@
+<?php
+//000000000000
+ exit();?>
+a:2:{s:6:"enable";a:1:{i:0;s:21:"\addons\wechat\Wechat";}s:7:"disable";a:1:{i:0;s:21:"\addons\wechat\Wechat";}}
